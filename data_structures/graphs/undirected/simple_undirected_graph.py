0b49acebe3e10a939ae0c9de6de0a8bebb0ba66f
AmitChaubey <amit.katyayana@gmail.com>
2025-04-30 21:20:15 +0100

    Add simple undirected graph implementation
    - Basic graph using adjacency list
    - Core operations: add/remove nodes and edges
    - Simple visualization with print_graph method

---
+++ data_structures/graphs/undirected/simple_undirected_graph.py
@@ -0,0 +1,39 @@
+class Graph:
+    def __init__(self):
+        self.graph = {}
+
+    def add_node(self, node):
+        if node not in self.graph:
+            self.graph[node] = []
+
+    def add_edge(self, node1, node2):
+        if node1 not in self.graph:
+            self.add_node(node1)
+        if node2 not in self.graph:
+            self.add_node(node2)
+
+        self.graph[node1].append(node2)
+        self.graph[node2].append(node1)
+
+    def remove_node(self, node):
+        if node in self.graph:
+            del self.graph[node]
+            for edges in self.graph.values():
+                if node in edges:
+                    edges.remove(node)
+
+    def remove_edge(self, node1, node2):
+        if node1 in self.graph and node2 in self.graph:
+            self.graph[node1].remove(node2)
+            self.graph[node2].remove(node1)
+ 
+    def print_graph(self):
+        for node in self.graph:
+            print(f"{node}--->{self.graph[node]}")
+
+
+if __name__=="__main__":
+    graph = Graph()
+    graph.add_edge("A", "B")
+    graph.add_edge("A", "C")
+    graph.print_graph()
