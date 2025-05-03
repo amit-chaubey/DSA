2c62fde0b924e95cb8676244fc0b0b7ec4b1c060
AmitChaubey <amit.katyayana@gmail.com>
2025-05-03 13:46:49 +0100

    Add singly linked list implementation

---
+++ data_structures/linear/linked_list/single_linkedlist.py
@@ -0,0 +1,47 @@
+class Node:
+    def __init__(self,data,next=None):
+        self.data = data
+        self.next = None
+
+class SingleLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self,data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node
+
+    def insert_at_position(self,data,position):
+        new_node = Node(data)
+        if position == 0:
+            new_node.next = self.head
+            self.head = new_node
+        else:
+            current = self.head
+            for i in range(position-1):
+                current = current.next
+            new_node.next = current.next
+            current.next = new_node
+
+    def print_ll(self):
+        current = self.head
+        while current:
+            print(current.data,end="->")
+            current = current.next
+        print("None")
+    
+if __name__=="__main__":
+    ll = SingleLinkedList()
+    ll.append(1)
+    ll.append(2)
+    ll.append(3)
+    ll.insert_at_position(4,2)
+    print(ll.head.data)
+
+    ll.print_ll()
