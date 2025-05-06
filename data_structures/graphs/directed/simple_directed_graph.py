19ae6748494cb48431ecc178883eb0cfc3f71238
AmitChaubey <amit.katyayana@gmail.com>
2025-05-07 00:28:50 +0100

    added better error handling

--- data_structures/graphs/directed/simple_directed_graph.py
+++ data_structures/graphs/directed/simple_directed_graph.py
@@ -3,12 +3,14 @@ class DirectedGraph:
         self.graph = {}
     
     def add_node(self,node):
-        if node not in self.graph:
+        if node not in self.graph or node not in self.graph.values():
             self.graph[node] = []
 
     def add_edge(self,node1,node2):
         if node1 in self.graph and node2 in self.graph:
             self.graph[node1].append(node2)
+        else:
+            print("Node not found)")
 
     def remove_node(self,node):
         if node in self.graph:
